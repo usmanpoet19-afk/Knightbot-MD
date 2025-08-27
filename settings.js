ba702688439fd21bac1b23ec73cb1b005cc53c80
Usman poet <usmanpoet19@gmail.com>
2025-08-28 02:13:24 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-number settings = {
+ownerNumber settings = {
   ,
   botName: "Knight Bot",
   botOwner: "usmanpoet"
-  ownerNumber: '+923161488992', '923213597724',//Set your number here without + symbol, just add country code & number without any space
+  ownerNumber:  '+923161488992', '923213597724',//Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",https://whatsapp.com/channel/0029VbAwoaqEKyZQMjxEYy3I
